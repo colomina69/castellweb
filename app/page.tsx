6dde00be2504e2948496b1dd57715987faac0a00
colomina69 <42197982+colomina69@users.noreply.github.com>
2025-11-11 16:59:50 +0100

    cuart

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { HeroHeader } from '@/components/header'
+import HeroSection from '@/components/hero-section'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Button>Bienvenido</Button>
-
+      <HeroSection />
     </div>
   )
 }
