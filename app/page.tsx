6ab773edd9c07bf7e380db8b52ac809ff24fe5d3
colomina69 <42197982+colomina69@users.noreply.github.com>
2025-11-12 12:05:35 +0100

    quint

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <HeroHeader />
       <HeroSection />
     </div>
   )
