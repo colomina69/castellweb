6ed98c7f5003fbd7699f341706a831e1e338ded1
colomina69 <42197982+colomina69@users.noreply.github.com>
2025-11-21 16:28:14 +0100

    imagen

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Image
+        className="mb-3"
+        src="/escudfila.png"
+        alt="Escudo Fila"
+        width={112}
+        height={112}
+        priority
+      />
       <Link href="/login">
         <Button className="m-4">Iniciar Sesión</Button>
       </Link>
